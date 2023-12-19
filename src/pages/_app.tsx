33f7ddf02de05c7455001fd979d40fc40a35d307
cmdnguyen <cmdnguyen94@gmail.com>
2023-12-19 09:52:15 -0600

    Fixed the default theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,21 +4,28 @@ import { Fragment } from "react";
 import Footer from "../components/Footer";
 // import '../app/globals.css'
 import Navbar from "../components/Navbar";
-import { ChakraProvider, chakra } from "@chakra-ui/react";
+import { ChakraProvider, chakra, ColorModeProvider } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Fragment>
-        <Head>
-          <title>TNTT Liturgy List</title>
-        </Head>
-      </Fragment>
-      <chakra.header id="header" bg="blue.600">
-        <Navbar />
-      </chakra.header>
-      <Component {...pageProps} />
-      <Footer />
+      <ColorModeProvider
+        options={{
+          initialColorMode: "dark",
+          useSystemColorMode: false,
+        }}
+      >
+        <Fragment>
+          <Head>
+            <title>TNTT Liturgy List</title>
+          </Head>
+        </Fragment>
+        <chakra.header id="header" bg="blue.600">
+          <Navbar />
+        </chakra.header>
+        <Component {...pageProps} />
+        <Footer />
+      </ColorModeProvider>
     </ChakraProvider>
   );
 }
