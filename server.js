4cbd19f3eff07fee8e7be9d80fa99159f9347374
cmdnguyen <cmdnguyen94@gmail.com>
2023-12-22 16:59:32 -0600

    Reverted Axios response

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ app.use(express.json());
 
 app.get('/api/liturgy', async (req, res) => {
   try {
-    const response = await axios.get('https://liturgy-checklist.vercel.app/api/liturgy');
+    const response = await axios.get('http://calapi.inadiutorium.cz/api/v0/en/calendars/default/today', { withCredentials: true });
+    console.log('Axios response data:', response.data);
     res.json(response.data);
   } catch (error) {
     console.error('Error making API request:', error.message);
