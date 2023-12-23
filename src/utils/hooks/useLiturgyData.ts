5dffef140ef9faafdfb4e372eef8fe56ba809328
cmdnguyen <cmdnguyen94@gmail.com>
2023-12-22 18:21:29 -0600

    Fixed useLiturgyData hook

--- src/utils/hooks/useLiturgyData.ts
+++ src/utils/hooks/useLiturgyData.ts
@@ -2,35 +2,44 @@
 import { useState, useEffect } from 'react';
 import { makeApiRequest } from '../api/liturgyAPI';
 
+export const fetchLiturgyData = async () => {
+  try {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+    const day = currentDate.getDate().toString().padStart(2, '0');
+    const formattedDate = `${year}-${month}-${day}`;
+
+    const endpoint = '/calendars/default/today';
+    const data = await makeApiRequest(endpoint);
+
+    return { data, formattedDate };
+  } catch (error) {
+    console.error('Error fetching liturgy data:', error);
+    throw error;
+  }
+};
+
 export const useLiturgyData = () => {
-    const [liturgyData, setLiturgyData] = useState<any>(null);
-    const [liturgicalSeason, setLiturgicalSeason] = useState<string>('');
-  
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const currentDate = new Date();
-          const year = currentDate.getFullYear();
-          const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
-          const day = currentDate.getDate().toString().padStart(2, '0');
-          const formattedDate = `${year}/${month}/${day}`;
-  
-          // Fetch data from liturgyCalendarAPI using the makeApiRequest function
-          const endpoint = `/calendars/default/${formattedDate}`;
-          const data = await makeApiRequest(endpoint);
-  
-          setLiturgyData(data);
-          console.log(data);
-  
-          const season = data.season || '';
-          setLiturgicalSeason(season.toLowerCase());
-        } catch (error) {
-          console.error('Error fetching liturgy data:', error);
-        }
-      };
-  
-      fetchData();
-    }, []);
+  const [liturgyData, setLiturgyData] = useState<any>(null);
+  const [liturgicalSeason, setLiturgicalSeason] = useState<string>('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data, formattedDate } = await fetchLiturgyData();
+        setLiturgyData(data);
+        console.log(data);
+        const season = data.season || '';
+        setLiturgicalSeason(season.toLowerCase());
+      } catch (error) {
+        console.error('Error fetching liturgy data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   
     const getColorScheme = () => {
       if (!liturgyData || !liturgyData.celebrations || liturgyData.celebrations.length === 0) {
