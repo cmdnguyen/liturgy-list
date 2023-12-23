395fe38b70634ded44bccd9424645fabbc618885
cmdnguyen <cmdnguyen94@gmail.com>
2023-12-22 19:25:44 -0600

    Fixed pages/api/liturgy

--- src/pages/api/liturgy.ts
+++ src/pages/api/liturgy.ts
@@ -1,11 +1,11 @@
 // pages/api/liturgy.ts
 import { NextApiRequest, NextApiResponse } from 'next';
-import { useLiturgyData } from '../../utils/hooks/useLiturgyData';
+import { fetchLiturgyData } from '../../utils/hooks/useLiturgyData';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const { liturgyData } = useLiturgyData(); // Use the hook directly
-    res.json(liturgyData);
+    const { data } = await fetchLiturgyData(); // Use the fetchLiturgyData function
+    res.json(data);
   } catch (error: unknown) {
     console.error('Error making API request:', (error as Error).message);
     res.status(500).json({ error: 'Internal Server Error' });
