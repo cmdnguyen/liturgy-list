d5499d573d3e76d88437a4bba6ba855f77664331
cmdnguyen <cmdnguyen94@gmail.com>
2023-12-22 21:00:23 -0600

    Debugging makeApiRequest

--- src/utils/api/liturgyAPI.ts
+++ src/utils/api/liturgyAPI.ts
@@ -4,6 +4,7 @@ import axios from 'axios';
 export const makeApiRequest = async (endpoint: string) => {
   try {
     const response = await axios.get(`http://calapi.inadiutorium.cz/api/v0/en${endpoint}`);
+    console.log('response:', response);
     return response.data;
   } catch (error) {
     console.error('Error making API request:', error);
