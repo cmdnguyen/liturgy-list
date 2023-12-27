15d1d18115dbd2bfb736d5df0e08ccf78a541b67
cmdnguyen <cmdnguyen94@gmail.com>
2023-12-26 20:47:39 -0600

    Redoing dailyReadings

--- src/pages/api/dailyReadings.ts
+++ src/pages/api/dailyReadings.ts
@@ -1,15 +1,67 @@
 // pages/api/dailyReadings.ts
 import { NextApiRequest, NextApiResponse } from 'next';
-import { getCatholicDailyReadings, DailyReadings } from 'get-catholic-daily-readings';
+import axios from 'axios';
+import cheerio from 'cheerio';
+import { format } from 'date-fns';
+
+interface Reading {
+  header: string;
+  reference: string;
+  rawText: string;
+  nonFormattedText: string;
+  formattedText: string;
+}
+
+interface DailyReadings {
+  header: string;
+  lectionary: number;
+  readings: Reading[];
+}
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     const currentDate = new Date();
-    const readings: DailyReadings = await getCatholicDailyReadings(currentDate);
+    const formattedDate = format(currentDate, 'MMddyy');
+    const url = `https://bible.usccb.org/bible/readings/${formattedDate}.cfm`;
+
+    const response = await axios.get<string>(url);
+    const readings = parseReadings(response.data);
 
     res.status(200).json({ readings });
   } catch (error: any) {
     console.error('Error fetching daily readings:', error);
     res.status(500).json({ error: 'Internal Server Error', details: error.message });
   }
+}
+
+function parseReadings(html: string): DailyReadings {
+  const $ = cheerio.load(html);
+
+  const lectionaryHeader = $('.b-lectionary div.innerblock h2').text().trim();
+  const lectionaryNumber = parseInt($('.b-lectionary div.innerblock p').text().match(/\d+/)?.[0] || '0');
+
+  const readingNodes = $('.b-verse');
+
+  const readings: Reading[] = readingNodes.map((index, element) => {
+    const header = $(element).find('div.content-header h3.name').text().trim();
+    const reference = $(element).find('div.address a').text().trim();
+    const rawText = $(element).find('div.content-body').html()?.replace(/\&nbsp;/g, '').trim() || '';
+
+    // Additional formatting if needed
+    const formattedText = rawText.replace(/<br\s*[/]?>/gi, '\n').replace(/<[^>]*>/g, '');
+
+    return {
+      header,
+      reference,
+      rawText,
+      nonFormattedText: rawText.replace(/<br\s*[/]?>/gi, ' '),
+      formattedText,
+    };
+  }).get();
+
+  return {
+    header: lectionaryHeader,
+    lectionary: lectionaryNumber,
+    readings,
+  };
 }
